a9f5718205ba3bca6fe36834b3419d9f184828a0
Anahi Quintero <anahiquintero@MacBook-Air-de-Anahi.local>
2021-01-01 19:32:55 -0600

    Get time and display it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-alert('Holi');
+const clock = document.querySelector('.clock');
+const SECOND = 1000;
+
+/**
+ * Return a 2-digit number
+ *
+ * @param {Number} number - The number to be formatted.
+ */
+function formatTwoDigits(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+/**
+ * Get current time and update DOM.
+ */
+function getTime() {
+  const now = new Date();
+  const hour = formatTwoDigits(now.getHours());
+  const minute = formatTwoDigits(now.getMinutes());
+  const second = formatTwoDigits(now.getSeconds());
+
+  clock.textContent = `${hour}:${minute}:${second}`;
+}
+
+setInterval(getTime, SECOND)
